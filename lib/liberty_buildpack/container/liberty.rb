7051a658e9e0d7b8b17cdeb03dc2feccdbec0c9f
eroslaci <eroslaci007@gmail.com>
2014-10-14 16:29:07 +0200

    a

--- lib/liberty_buildpack/container/liberty.rb
+++ lib/liberty_buildpack/container/liberty.rb
@@ -478,7 +478,8 @@ module LibertyBuildpack::Container
 	def set_toolbox_variables
 		folders  = Dir.entries(@app_dir+"/wlp/usr/servers").select {|entry| File.directory? File.join(@app_dir+"/wlp/usr/servers",entry) and !(entry =='.' || entry == '..') }
 		print "\nFound server directory name: "+folders[0]
-		
+		print "\nDSMI_CONFIG: "+@app_dir+"/wlp/usr/servers/"+folders[0]+"/opt/tivoli/tsm/client/api/bin64/dsm.opt"
+		print "\nLD_LIBRARY_PATH: "+@app_dir+"/wlp/usr/servers/"+folders[0]+"//opt/tivoli/tsm/client/api/bin64:"+@app_dir+"/wlp/usr/servers/"+folders[0]+"/usr/local/ibm/gsk8_64/lib64:"+@app_dir+"/wlp/usr/servers/"+folders[0]+"/toolboxlib"
 		ENV['DSMI_CONFIG'] = @app_dir+"/wlp/usr/servers/"+folders[0]+"/opt/tivoli/tsm/client/api/bin64/dsm.opt"
 		ENV['DSMI_DIR'] = @app_dir+"/wlp/usr/servers/"+folders[0]+"/opt/tivoli/tsm/client/api/bin64/"
 		ENV['DSMI_LOG'] = @app_dir+"/wlp/usr/servers/"+folders[0]+"/log"
